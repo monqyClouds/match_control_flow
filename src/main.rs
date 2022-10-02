c35854d03534cf5e08692311be98666a771c8d4a
monqyclouds <marcellusarthurs@gmail.com>
2022-10-01 19:08:47 -0700

    add new functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+#[derive(Debug)]
+
+enum UseState {
+    Alabama,
+    Alaska,
+    California,
+    Massachussets,
+    // 'New York',
+    // North Carolina,
+    // North Dakota
+    Ohio,
+    // South Carolina,
+    // South Dakota,
+    Texas,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UseState),
 }
 
 fn main() {
@@ -11,7 +27,37 @@ fn main() {
             Coin::Penny => 1,
             Coin::Nickel => 5,
             Coin::Dime => 10,
-            Coin::Quarter => 25,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
         }
     }
+
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
+    fn add_fancy_hat() {}
+    fn remove_fancy_hat() {}
+    fn move_player(_num_spaces: u8) {}
+
+    value_in_cents(Coin::Quarter(UseState::Texas));
+    value_in_cents(Coin::Dime);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Penny);
 }
